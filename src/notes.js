3ac5017e69e7fb0a2480782b577a4e0c72c73913
Muhammad Alvin <mhdalvin@gmail.com>
2021-05-25 10:34:35 +0700

    Update default data

--- src/notes.js
+++ src/notes.js
@@ -1,7 +1,7 @@
 const notes = [
     {
         id: 'lipsum1',
-        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        title: 'Lorem Ipsum',
         tags: ['lipsum', 'lorem', 'ipsum'],
         body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel eros neque. Quisque vestibulum mauris felis, sed fringilla nunc ultricies at. Quisque a nisl sit amet augue tincidunt tristique at ornare dolor. Proin magna leo, aliquet a rhoncus suscipit, ultrices quis sapien. In quis sollicitudin mauris, non pretium leo. Cras at accumsan ipsum, in facilisis nunc. Cras ac scelerisque lacus, finibus feugiat nulla.',
         createdAt: '2000-01-01T00:00:00.000Z',
